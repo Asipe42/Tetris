d54300858fbb515b50ec34fe54c9302fe0c35ddb
Modin <eom3658@gmail.com>
2024-03-16 15:28:28 +0900

    [개발/엄재천] OMino 구조 개발

--- Tetris/Source.cpp
+++ Tetris/Source.cpp
@@ -1,11 +1,76 @@
 #include <iostream>
 #include <vector>
 #include <windows.h>
+#include <cassert>
 
 using namespace std;
 
 const int ROW = 22;
 const int COLUMN = 24;
+
+struct Point
+{
+public:
+	int x;
+	int y;
+
+	Point()
+	{
+		this->x = 0;
+		this->y = 0;
+	}
+
+	Point(int x, int y)
+	{
+		this->x = x;
+		this->y = y;
+	}
+};
+
+struct Block
+{
+public:
+	Point leftPoint;
+	Point rightPoint;
+
+	Block()
+	{
+		leftPoint = Point();
+		rightPoint = Point();
+	}
+
+	Block(Point startPoint)
+	{
+		leftPoint = Point(startPoint.x, startPoint.y);
+		rightPoint = Point(startPoint.x + 1, startPoint.y);
+	}
+};
+
+class OMino
+{
+private:
+	Block blocks[4];
+
+public:
+	OMino(Point startPoint)
+	{
+		Point p1 = Point(startPoint.x + 0, startPoint.y + 0);
+		Point p2 = Point(startPoint.x + 2, startPoint.y + 0);
+		Point p3 = Point(startPoint.x + 0, startPoint.y + 1);
+		Point p4 = Point(startPoint.x + 2, startPoint.y + 1);
+
+		blocks[0] = Block(p1);
+		blocks[1] = Block(p2);
+		blocks[2] = Block(p3);
+		blocks[3] = Block(p4);
+	}
+
+	const Block* GetBlocks()
+	{
+		return blocks;
+	}
+};
+
 
 int main()
 {
@@ -42,14 +107,17 @@ int main()
 		vector<char> {' ', ' ', '\\', '/', '\\', '/', '\\', '/', '\\', '/', '\\', '/', '\\', '/', '\\', '/', '\\', '/', '\\', '/', '\\', '/', ' ', ' ' }
 	};
 
-	// 규칙 1
-	// '[' 블럭은 홀수에 와야하고
-	// ']' 블럭은 짝수에 와야한다.
+	Point startPoint = Point(4, 1);
+	OMino oMino = OMino(startPoint);
 
-	grid[0][5] = '['; grid[0][6] = ']';
-	grid[0][8] = '['; grid[0][9] = ']';
-	grid[1][6] = '['; grid[1][7] = ']';
-	grid[1][8] = '['; grid[1][9] = ']';
+	const Block* blocks = oMino.GetBlocks();
+
+	for (int i = 0; i < 4; i++)
+	{
+		grid[blocks[i].leftPoint.y][blocks[i].leftPoint.x] = '[';
+		grid[blocks[i].rightPoint.y][blocks[i].rightPoint.x] = ']';
+	}
+
 
 	while (true)
 	{
